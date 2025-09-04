a4dfc6df4f68bcee1eb3c0cdb03d577ab59f9a28
genki <g.lopessatanacruz@gmail.com>
2025-09-04 16:39:09 -0300

    push

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok", "message": "API funcionando!"}
+
+@app.get("/me")
+def about_me():
+    return {
+        "nome": "guilherme lopes",
+        "email": "seuemail@example.com",
+        "curso": "Si",
+        "github": "https://github.com/seuusuario",
+        "cidade": "Sua cidade",
+        "interesses": ["Python", "APIs", "FastAPI", "Tecnologia , meme"]
+    }
